debf7104a965315001b3cee396d3bafb6998ff58
MunisaIUT <muniazim89@gmail.com>
2020-05-04 23:29:50 +0500

    First view for index page

---
+++ resources/views/extra/carousel.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('content1')
+
+	<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/12.jpg" class="" alt="..." style="width:100%; height:550px;">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Stay with us</h5>
+						<p>You can find a new job</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/6.jpg" class="d-block" alt="..." style="width:100%; height:550px;">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Follow us</h5>
+						<p>Everyone can find a dream job</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/19.jpg" class=" d-block w-100" alt="..." style="width:100%; height:550px;">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Discover us</h5>
+						<p>Diversity of jobs for anyone</p>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+@endsection
