712cbf0003c09ec58d3578807918186d85da36ca
ulugbekabirov <ulugbekabirov@gmail.com>
2020-05-08 21:53:52 +0500

    seeders Added

--- database/seeds/JobsTableSeeder.php
+++ database/seeds/JobsTableSeeder.php
@@ -13,10 +13,34 @@ class JobsTableSeeder extends Seeder
     public function run()
     {
         //
-        $post = new Post([
-            'title' => 'Lorem',
-            'body' => 'heree'
+        // $post = new Post([
+        //     'title' => 'Lorem',
+        //     'body' => 'heree'
+        // ]);
+        // $post->save();
+
+
+        DB::table('posts')->insert([
+            'title' => 'Software Engineer',
+            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laboriosam error blanditiis alias voluptatem, saepe sit quaerat ipsam qui ea magnam aliquid est, delectus vitae. Accusantium vero minima, voluptate aperiam.',
+            'created_at' => '2012-12-12 12:12:12',
+            'updated_at' => '2012-12-12 12:12:12'
         ]);
-        $post->save();
+
+     DB::table('posts')->insert([
+            'title' => 'Project Manager',
+            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laboriosam error blanditiis alias voluptatem, saepe sit quaerat ipsam qui ea magnam aliquid est, delectus vitae. Accusantium vero minima, voluptate aperiam.',
+            'created_at' => '2012-12-12 12:12:12',
+            'updated_at' => '2012-12-12 12:12:12'
+        ]);
+
+      DB::table('posts')->insert([
+            'title' => 'Teacher',
+            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laboriosam error blanditiis alias voluptatem, saepe sit quaerat ipsam qui ea magnam aliquid est, delectus vitae. Accusantium vero minima, voluptate aperiam.',
+            'created_at' => '2012-12-12 12:12:12',
+            'updated_at' => '2012-12-12 12:12:12'
+        ]);
+
+
     }
 }
